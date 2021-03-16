a2f01facd7fe5b7986431bc95fba1af07004f1bd
shadya7med <57808802+shadya7med@users.noreply.github.com>
2021-03-17 01:44:22 +0200

    first

--- app/src/main/java/com/iti/example/tripreminder/MainActivity.java
+++ app/src/main/java/com/iti/example/tripreminder/MainActivity.java
@@ -1,8 +1,8 @@
 package com.iti.example.tripreminder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
