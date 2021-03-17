c85ddb2ce34eb23c9cd0b96fcd6cb4d5b157d620
shadya7med <57808802+shadya7med@users.noreply.github.com>
2021-03-17 23:43:29 +0200

    activites files are added

---
+++ app/src/main/java/com/iti/example/tripreminder/PastTripsActivity.java
@@ -0,0 +1,14 @@
+package com.iti.example.tripreminder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PastTripsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_trips);
+    }
+}
---
+++ app/src/main/java/com/iti/example/tripreminder/ReminderService.java
@@ -0,0 +1,16 @@
+package com.iti.example.tripreminder;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class ReminderService extends Service {
+    public ReminderService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
